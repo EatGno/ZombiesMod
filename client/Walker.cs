e66e784e03a0ce49d0293765d95414d5a736c2b7
EatGno <152440715+EatGno@users.noreply.github.com>
2024-12-25 17:44:22 +0100

    Create Walker.cs

---
+++ client/Walker.cs
@@ -0,0 +1,50 @@
+using GTA;
+
+namespace ZombiesMod.Zombies.ZombieTypes
+{
+    public class Walker : ZombiePed
+    {
+        private readonly Ped _ped;
+
+        public Walker(int handle) : base(handle)
+        {
+            this._ped = (Ped)((ZombiePed)this);
+        }
+
+        public override string MovementStyle { get; set; } = "move_m@drunk@verydrunk";
+
+        public override void OnAttackTarget(Ped target)
+        {
+            if (((Entity)target).get_IsDead())
+            {
+                if (((Entity)this._ped).IsPlayingAnim("amb@world_human_bum_wash@male@high@idle_a", "idle_b"))
+                    return;
+
+                this._ped.get_Task().PlayAnimation("amb@world_human_bum_wash@male@high@idle_a", "idle_b", 8f, -1, (AnimationFlags)1);
+            }
+            else
+            {
+                if (((Entity)this._ped).IsPlayingAnim("rcmbarry", "bar_1_teleport_aln"))
+                    return;
+
+                this._ped.get_Task().PlayAnimation("rcmbarry", "bar_1_teleport_aln", 8f, 1000, (AnimationFlags)16);
+                
+                if (!((Entity)target).get_IsInvincible())
+                    target.ApplyDamage(ZombiePed.ZombieDamage);
+
+                InfectTarget(target);
+            }
+        }
+
+        public override void OnGoToTarget(Ped target)
+        {
+            this._ped.get_Task().GoTo((Entity)target);
+        }
+
+         // Méthode pour infecter une cible (à ajouter ici ou dans ZombiePed)
+         private void InfectTarget(Ped target)
+         {
+             // Logique d'infection ici...
+         }
+    }
+}
