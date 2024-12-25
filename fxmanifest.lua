623faad28fd4d4c2ae05b97232a01b9d36d5647a
EatGno <152440715+EatGno@users.noreply.github.com>
2024-12-25 17:40:56 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,11 @@
 
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Votre Nom'
+description 'Mod de zombies pour FiveM'
+version '1.0.0'
+
+client_script 'client/ZombiePed.cs'
+client_script 'client/Walker.cs'
+server_script 'server/ZombieSpawner.cs'
